ef9c79a6276134706823ce6598f93bcfabce83d4
Justin Noeller <50307506+noellerjd@users.noreply.github.com>
2022-12-29 18:56:44 -0700

    Update main.py
    updated to show where the user stands on the BMI chart

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-height_round = round(float(height),2)
-weight_round = round(float(weight))
-bmi = weight_round / (height_round * height_round)
-print("Your BMI:")
-print(round(bmi))
+print("Welcome to the BMI Calculator.")
+
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+height_round = round(height,2)
+weight_round = round(weight)
+bmi = round(weight_round / (height_round * height_round))
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi > 35:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
